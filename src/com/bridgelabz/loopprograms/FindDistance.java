9ebe056f4e7904a2307ecf48056f3b972ba7e7ef
JAGADESH J <you@example.com>
2024-02-12 12:19:39 +0530

    First Commit

---
+++ src/com/bridgelabz/loopprograms/FindDistance.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.loopprograms;
+
+public class FindDistance {
+    public static void main(String[] args) {
+        int x =4;
+            int y = 4;
+            double distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+            System.out.println(distance);
+        }
+    }
+
+
